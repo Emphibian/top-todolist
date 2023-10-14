8ba2b1552c3c45eaf21d40c3a8c871d4bdb5dfd7
Emphibian <RyuuSleek@protonmail.ch>
2023-10-14 10:34:03 +0530

    Add css support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("min-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,5 +8,13 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
+  modules: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
 };
