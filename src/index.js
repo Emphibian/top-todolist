b5d45099b3631a6fce1b3a3dcbbfa1e58e1a8366
Emphibian <RyuuSleek@protonmail.ch>
2023-10-15 11:47:51 +0530

    Add js to generate the static page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+const body = document.querySelector('body');
+const mainDiv = document.createElement('main');
+
+const sidebarDiv = document.createElement('div');
+sidebarDiv.classList.add('sidebar');
+
+const defaultButton = document.createElement('button');
+defaultButton.textContent = 'Default';
+sidebarDiv.appendChild(defaultButton);
+
+const todoListDiv = document.createElement('div');
+todoListDiv.classList.add('todo-list');
+
+function createTodoItem(text) {
+  const itemDiv = document.createElement('div');
+  itemDiv.textContent = text;
+  itemDiv.classList.add('todo-item');
+  return itemDiv;
+}
+
+const addButton = document.createElement('button');
+addButton.classList.add('add-button');
+addButton.textContent = '+';
+
+todoListDiv.appendChild(createTodoItem('Do this'));
+todoListDiv.appendChild(createTodoItem('Do that'));
+todoListDiv.appendChild(addButton);
+
+mainDiv.appendChild(sidebarDiv);
+mainDiv.appendChild(todoListDiv);
+
+body.appendChild(mainDiv);
