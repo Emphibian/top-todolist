e0f46547c77c1525399566ff2a6a85733a985457
Emphibian <RyuuSleek@protonmail.ch>
2023-10-20 10:52:57 +0530

    Setup a source map

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: 'eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: "index",
