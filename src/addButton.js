bc4ce85721aa0c773400c1930faa17eea73780af
Emphibian <RyuuSleek@protonmail.ch>
2023-10-21 12:00:58 +0530

    Fix todo-list not grabbing full space

--- src/addButton.js
+++ src/addButton.js
@@ -1,6 +1,6 @@
 export default function createAddButton() {
   const addButton = document.createElement('button');
-  addButton.textContent = '-';
+  addButton.textContent = '+';
 
   addButton.addEventListener('click', () => {
     const dialogBox = document.querySelector('#addTask');
