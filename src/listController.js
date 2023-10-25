dd8e9159926b99d99a24d16968675f5597320c3f
Emphibian <RyuuSleek@protonmail.ch>
2023-10-25 10:22:04 +0530

    Add a listController file

---
+++ src/listController.js
@@ -0,0 +1,41 @@
+export default function listController() {
+  let curList;
+
+  function setList(list) {
+    curList = list;
+  }
+
+  function render() {
+    if (!curList) return;
+
+    let todoListDiv = document.querySelector(".todo-list");
+    todoListDiv.innerHTML = "";
+    curList.getItems().forEach((item, index) => {
+      const listItemDiv = document.createElement("div");
+      listItemDiv.classList.add('todo-item');
+
+      const titlePara = document.createElement('p');
+      titlePara.textContent = item.title;
+
+      const doneCheckbox = document.createElement('input');
+      doneCheckbox.type = 'checkbox';
+      doneCheckbox.addEventListener("click", () => {
+        curList.markDone(index);
+        render();
+      });
+
+      const addButton = createAddButton();
+
+      listItemDiv.appendChid(doneCheckbox);
+      listItemDiv.appendChid(titlePara);
+
+      todoListDiv.appendChild(listItemDiv);
+      todoListDiv.appendChild(addButton);
+    });
+  }
+
+  return {
+    setList,
+    render,
+  };
+}
